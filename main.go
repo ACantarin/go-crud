559c875d63ba512de4077b0062fbf96c041121ff
André Cantarin do Rosário <andrecantarin@gmail.com>
2025-10-02 01:00:54 -0300

    Cria conexão e lista produtos da tabela do banco Postgres

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
+	"os"
 	"text/template"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+/*
+Configura a conexão com o Postgres
+*/
+func dbConnect() *sql.DB {
+	/*
+		Carrega as variáveis de ambiente utilizando a lib godotenv
+	*/
+	err := godotenv.Load()
+	if err != nil {
+		os.Exit(-1)
+	}
+
+	dbPass := os.Getenv("POSTGRES_PASS")
+	connection := "user=postgres dbname=go_crud password=" + dbPass + " host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
 type Product struct {
+	Id          int
 	Name        string
 	Description string
 	Price       float64
@@ -35,28 +64,44 @@ O parâmetro "w" representa o ResponseWriter é responsável por escrever a resp
 O parâmetro "r" representa o Request, contendo os dados da requisição
 */
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Product{
-		{
-			Name:        "Camiseta",
-			Description: "Cor preta",
-			Price:       29.00,
-			Quantity:    10,
-		},
-		{
-			Name:        "Notebook",
-			Description: "16gb RAM",
-			Price:       1999.00,
-			Quantity:    2,
-		},
-		{
-			Name:        "Mouse",
-			Description: "Logitech",
-			Price:       49.90,
-			Quantity:    5,
-		},
-	}
 	/*
-		Executa o template "index.html" passando o parâmetro "nil" pois o template não executa nenhuma ação
+		Abre a conexão com o Postgres e fecha no fim da função após executá-la com o comando "defer"
 	*/
-	templates.ExecuteTemplate(w, "index.html", produtos)
+	db := dbConnect()
+
+	/*
+		Executa a query "SELECT * FROM products"
+	*/
+	rows, err := db.Query("SELECT * FROM products")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	product := Product{}
+	products := []Product{}
+
+	for rows.Next() {
+		var id, quantity int
+		var name, description string
+		var price float64
+
+		err = rows.Scan(&id, &name, &description, &price, &quantity)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		product.Name = name
+		product.Description = description
+		product.Price = price
+		product.Quantity = quantity
+
+		products = append(products, product)
+	}
+
+	/*
+		Executa o template "index.html" passando os dados de produtos como parâmetro ou "nil" se o template não executa nenhuma ação
+	*/
+	templates.ExecuteTemplate(w, "index.html", products)
+
+	defer db.Close()
 }
